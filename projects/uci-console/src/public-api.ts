79da15832d1c8a46b37cea42c09815c1a7dc6284
Manoj Singh Rawat <manojrawat1502@gmail.com>
2021-07-08 18:51:17 +0530

    Entery component added in api

--- projects/uci-console/src/public-api.ts
+++ projects/uci-console/src/public-api.ts
@@ -5,3 +5,4 @@
 export * from './lib/uci-module';
 export * from './lib/uci-events.service';
 export *  from './lib/models/discussion-config.model';
+export * from './lib/components/lib-entry/lib-entry.component';
